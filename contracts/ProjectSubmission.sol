c6d023ad392dadd48bf7f361bdbf5fa4549a63ba
Matthieu SCARSET <m@matthieuscarset.com>
2021-11-02 21:02:13 +0100

    Remove whitespaces

--- contracts/ProjectSubmission.sol
+++ contracts/ProjectSubmission.sol
@@ -1,5 +1,6 @@
 pragma... // Step 1
 
+
 contract ProjectSubmission { // Step 1
 
     ...owner... // Step 1 (state variable)
@@ -7,13 +8,13 @@ contract ProjectSubmission { // Step 1
     modifier onlyOwner() { // Step 1
       ...
     }
-    
+
     struct University { // Step 1
         ...available...
         ...balance...
     }
     ...universities... // Step 1 (state variable)
-    
+
     // enum ProjectStatus { ... } // Step 2
     // struct Project { // Step 2
     //     ...author...
@@ -22,35 +23,35 @@ contract ProjectSubmission { // Step 1
     //     ...balance...
     // }
     // ...projects... // Step 2 (state variable)
-    
+
     function registerUniversity... { // Step 1
       ...
     }
-    
+
     function disableUniversity... { // Step 1
       ...
     }
-    
+
     // function submitProject... { // Step 2 and 4
     //   ...
     // }
-    
+
     // function disableProject... { // Step 3
     //   ...
     // }
-    
+
     // function reviewProject... { // Step 3
     //   ...
     // }
-    
+
     // function donate... { // Step 4
     //   ...
     // }
-    
+
     // function withdraw... { // Step 5
     //   ...
     // }
-    
+
     // function withdraw... {  // Step 5 (Overloading Function)
     //   ...
     // }
