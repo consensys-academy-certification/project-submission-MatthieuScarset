69378b007b29d8bc7e15a8dc7cc3bc120ded1193
Matthieu SCARSET <m@matthieuscarset.com>
2021-11-15 21:13:34 +0100

    Update truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -1,17 +1,16 @@
 // ===== DO NOT MODIFY THIS FILE =====
 
-module.exports = { 
+module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*"
-    }
+      network_id: "*",
+    },
   },
   compilers: {
     solc: {
-      version: "^0.5.0", // A version or constraint - Ex. "^0.5.0"
-                         // Can also be set to "native" to use a native solc
-      }
-    }
-}
+      version: "^0.8.0",
+    },
+  },
+};
